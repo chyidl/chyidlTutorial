a5581e7ad3a82eb40af1a5674330b8dd8534b750
chyidl <chyidl.com@gmail.com>
2018-11-13 23:00:30 +0800

    bitree.c

--- root/os/DSAA/MasteringAlgorithmsWithC/MasterAlgorithmsWithC/source/bitree.c
+++ root/os/DSAA/MasteringAlgorithmsWithC/MasterAlgorithmsWithC/source/bitree.c
@@ -12,31 +12,6 @@
 #include "bitree.h"
 
 /*******************************************************************
-* Define a structure for binary tree nodes. 
-*******************************************************************/
-
-typedef struct BiTreeNode_ {
-    
-    void                *data;
-    struct BiTreeNode_  *left;
-    struct BiTreeNode_  *right;
-
-} BiTreeNode;
-
-/**
- * Define a structure for binary trees.
- */
-typedef struct BiTree_ {
-
-    int         size;
-
-    int         (*compare)(const void *key1, const void *key2);
-    void        (*destroy)(void *data);
-
-    BiTreeNode  *root;
-} BiTree;
-
-/*******************************************************************
 * Public Interface
 *******************************************************************/
 
@@ -50,7 +25,14 @@ typedef struct BiTree_ {
  *
  * @return None
  * */
-void bitree_init(BiTree *tree, void (*destroy)(void *data));
+void bitree_init(BiTree *tree, void (*destroy)(void *data)){
+    // Initialize the binary tree.
+    tree->size = 0;
+    tree->destroy = destroy;
+    tree-root = NULL;
+
+    return ;
+}
 
 
 /**
@@ -62,7 +44,15 @@ void bitree_init(BiTree *tree, void (*destroy)(void *data));
  *
  * @return None 
  * */
-void bitree_destroy(BiTree *tree);
+void bitree_destroy(BiTree *tree) {
+    // Remove all the nodes from the tree.
+    bitree_rem_left(tree, NULL);
+
+    // No operations are allowed now, but clear the structure as a precaution.
+    memset(tree, 0, sizeof(BiTree));
+    
+    return ;
+}
 
 
 /**
@@ -76,7 +66,40 @@ void bitree_destroy(BiTree *tree);
  *
  * @return 0 if inserting the element is successful, or -1 otherwise.
  * */
-int bitree_ins_left(BiTree *tree, BiTreeNode *node, const void *data);
+int bitree_ins_left(BiTree *tree, BiTreeNode *node, const void *data){
+    BiTreeNode *new_node, **position;
+
+    // Determine where to insert the node.
+    if (node == NULL) {
+        // Allow insertion at the root only in an empty tree.
+        if (bitree_size(tree) > 0)
+            return -1;
+
+        position = &tree->root;
+    }
+    else {
+        // Normally allow insertion only at the end of a branch,
+        if (bitree_left(node) != NULL)
+            return -1;
+
+        position = &node->left;
+    }
+
+    // Allocate storage for the node.
+    if ((new_node = (BiTreeNode *)malloc(sizeof(BiTreeNode))) == NULL)
+        return -1;
+
+    // Insert the node into the tree 
+    new_node->data = (void *)data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    *position = new_node;
+
+    // Adjust the size of the tree to account for the inserted node. 
+    tree->size++;
+    
+    return 0;
+}
 
 
 /**
@@ -90,7 +113,41 @@ int bitree_ins_left(BiTree *tree, BiTreeNode *node, const void *data);
  *
  * @return 0 if inserting the element is successful, or -1 otherwise.
  * */
-int bitree_ins_right(BiTree *tree, BiTreeNode *node, const void *data);
+int bitree_ins_right(BiTree *tree, BiTreeNode *node, const void *data){
+    BiTreeNode *new_node, **position;
+
+    // Determine where to insert the node 
+    if (node == NULL) {
+        // Allow insertion at the root only in an empty tree.
+        if(bitree_size(tree) > 0)
+            return -1;
+
+        position = &tree->root;
+
+    }
+    else {
+        // Normally allow insertion only at the end of a branch 
+        if (bitree_right(node) != NULL)
+            return -1;
+        
+        position = &node->right;
+
+    }
+    // Allocate storage for the node.
+    if ((new_node = (BiTreeNode *)malloc(sizeof(BiTreeNode))) == NULL)
+        return -1;
+
+    // Insert the node into the tree.
+    new_node->data = (void *)data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    *position = new_node;
+
+    // Adjust the size of the tree to account for the inserted node. 
+    tree->size++;
+
+    return 0;
+}
 
 
 /**
@@ -103,7 +160,37 @@ int bitree_ins_right(BiTree *tree, BiTreeNode *node, const void *data);
  *
  * @return None
  * */
-int bitree_rem_left(BiTree *tree, BiTreeNode *node);
+int bitree_rem_left(BiTree *tree, BiTreeNode *node){
+    BiTreeNode **position;
+
+    // Do not allow removal from an empty tree.
+    if (bitree_size(tree) == 0)
+        return;
+
+    // Determine where to remove nodes.
+    if (node == NULL)
+        position = &tree->root;
+    else
+        position = &node->left;
+
+    // Remove the nodes
+    if (*position != NULL) {
+        bitree_rem_left(tree, *position);
+        bitree_rem_right(tree, *position);
+
+        if (tree->destroy != NULL) {
+            // Call a user-defined function to free dynamically allocated data. 
+            tree->destroy((*position)->data);
+        }
+        
+        free(*position);
+        *position = NULL;
+        
+        // Adjust the size of the tree to account for the removed node. 
+        tree->size--;
+    }
+    return ;
+}
 
 
 /**
@@ -116,7 +203,37 @@ int bitree_rem_left(BiTree *tree, BiTreeNode *node);
  *
  * @return None
  * */
-int bitree_rem_right(BiTree *tree, BiTreeNode *node);
+int bitree_rem_right(BiTree *tree, BiTreeNode *node){
+    BiTreeNode **position;
+
+    // Do not allow removal from an empty tree.
+    if (bitree_size(tree) == 0)
+        return;
+
+    // Determine where to remove nodes.
+    if (node == NULL)
+        position = &tree->root;
+    else
+        position = &node->right;
+
+    // Remove the nodes 
+    if (*position != NULL) {
+        bitree_rem_left(tree, *position);
+        bitree_rem_right(tree, *position);
+
+        if (tree->destroy != NULL) {
+            // Call a user-defined function to free dynamically allocated data. 
+            tree->destroy((*position)->data);
+        }
+        free(*position);
+        *position = NULL;
+
+        // Adjust the size of the tree to account for the removed node. 
+        tree->size--;
+
+    }
+    return;
+}
 
 /**
  * Merges the two binary trees specified by left and right into the single binary tree merge.
@@ -130,76 +247,28 @@ int bitree_rem_right(BiTree *tree, BiTreeNode *node);
  *
  * @return 0 if merging the trees is successful, or -1 otherwise
  * */
-int bitree_merge(BiTree *merge, BiTree *left, BiTree *right, const void *data);
+int bitree_merge(BiTree *merge, BiTree *left, BiTree *right, const void *data){
+    // Initialize the merged tree.
+    bitree_init(merge, left->destroy);
 
+    // Insert the data for the root node of the merged tree.
+    if (bitree_ins_left(merge, NULL, data) != 0) {
+        bitree_destroy(merge);
+        return -1;
+    }
 
-/**
- * Macro that evaluates to the number of nodes in the binary tree specified by tree
- *
- * @Complexity O(1)
- *
- * @return Number of nodes in the tree
- * */
-#define bitree_size(tree) ((tree)->size)
+    // Merge the two binary trees into a single binary tree. 
+    bitree_root(merge)->left = bitree_root(left);
+    bitree_root(merge)->right = bitree_root(right);
 
+    // Adjust the size of the new binary tree. 
+    merge->size = merge->size + bitree_size(left) + bitree_size(right);
 
-/**
- * Macro that evaluates to the node at the root of binary the tree specified by tree.
- *
- * @Complexity O(1)
- *
- * @return Element at the root of the tree
- * */
-#define bitree_root(tree) ((tree)->root)
+    // Do not let the original trees access the merged nodes. 
+    left->root = NULL;
+    left->size = 0;
+    right->root = NULL;
+    right->size = 0;
 
-
-/**
- * Macro that determines whether the node specified as node marks the end of a branch in a binary tree.
- *
- * @Complexity O(1)
- *
- * @return  1 if the node marks the end of a branch, or 0 otherwise. 
- * */
-#define bitree_is_eob(node) ((node) == NULL)
-
-
-/**
- * Macro that determines whether the node specified as node is a leaf node in a binary tree.
- *
- * @Complexity O(1)
- *
- * @return 1 if the node is a leaf node, or 0 otherwise.
- * */
-#define bitree_is_leaf(node) ((node)->left == NULL && (node)->right == NULL)
-
-
-/**
- * Macro that evaluates to the data stored in the node of a binary tree specified by node.
- *
- * @Complexity O(1)
- *
- * @return Data stored in the node.
- * */
-#define bitree_data(node) ((node)->data)
-
-
-/**
- * Macro that evaluates to the node of a binary tree that is the left child of the node specifed by node.
- *
- * @Complexity O(1)
- *
- * @return Left child of the specified node.
- * */
-#define bitree_left(node) ((node)->left)
-
-
-/**
- * Macro that evaluates to the node of a binary tree that is the right child of the node specifed by node.
- *
- * @Complexity O(1)
- *
- * @return Right child of the specified node.
- * */
-#define bitree_right(node) ((node)->right)
-
-#endif /* bitree_h */
+    return 0;
+}
