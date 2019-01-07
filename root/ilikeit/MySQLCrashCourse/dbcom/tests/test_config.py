8e069c345f5e4aad0f08e9b5526b00999e7cc322
chyidl <44197301+chyidl@users.noreply.github.com>
2019-01-07 12:31:44 +0800

    Update test_config.py

--- root/ilikeit/MySQLCrashCourse/dbcom/tests/test_config.py
+++ root/ilikeit/MySQLCrashCourse/dbcom/tests/test_config.py
@@ -4,12 +4,12 @@
 # MySQL & MariaDB
 mysql_host='192.168.82.56'
 mysql_port=3306
-mysql_user='chyidl'
-mysql_passwd='macintosh'
+mysql_user='xxx'
+mysql_passwd='xxx'
 
 
 # MsSQL
 mssql_host='192.168.188.20'
 mssql_port=1433
-mssql_user='qiyaqing'
-mssql_passwd='qyq.2017'
+mssql_user='xxx'
+mssql_passwd='xxxxxx'
