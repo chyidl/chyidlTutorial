8c3ee14ff1678ef04e222a97ccffd3be9ef8d567
chyidl <chyidl.com@gmail.com>
2019-02-20 22:53:17 +0800

    Add skip list Algorithm, so hard

--- root/os/DSAA/DataStructuresAndAlgorithms/python/skip_list_implement.py
+++ root/os/DSAA/DataStructuresAndAlgorithms/python/skip_list_implement.py
@@ -25,15 +25,33 @@
 """
 Skip List: create multiple layers so that we can skip some nodes.
 
+The time complexity of skip lists can be reduced further by adding more layers.
+In fact, the time complexity of search, insert and delete can become O(logn) in
+average case with O(n) extra space.
+
 Insert an Elements in Skip List
+
+Deciding nodes level:
+    randomLevel()
+    lvl := 1
+    // random() that returns a random value in [0...1]
+    while random() < p and lvl < MaxLevel do
+    lvl := lvl + 1
+    return lvl
+
+Node Structure
+    Each node carries a key and a forward array carring pointers to nodes of a
+    different level. A level i node carries i forward pointers indexed through
+    0 to i
+
+Insertion in Skip List
     start from highest level in the list and compare key of next node of the
-    current node with the key to be inserted.
-    1) Key of next node is less than key to be inserted then we keep on moving
-    forward on the same level
-    2) key of next node is greater than the key to be inserted then we store
-    the pointer to current node i at update[i] and move one level down and
-    continue our search
-    At the level 0, we will definitely find a position to insert given key.
+    current node with the ley to be inserted. Basic idea is if -
+        1. Key of next node is less than key to be inserted then we keep on
+        moving forward on the same level
+        2. Key of next node is greater than the key to be inserted then we
+        store the pointer to current node i at update[i] and move one level
+        down and continue our search
 
 Following is the psuedo code for the insertion algorithm
     Insert(list, searchKey)
@@ -41,28 +59,28 @@ Following is the psuedo code for the insertion algorithm
     x := list->header
     for i := list-level downto 0 do
 """
-import random
+# import random
 
 
 # Python3 code for inserting element in skip list
 class Node:
-    # Constructor to create a new node
+    # Class to implement node
     def __init__(self, key, level):
         self.key = key
 
-        # list to hold references to node of different level
+        # list to hold references to node of different level(level from 0)
         self.forward = [None]*(level+1)
 
 
 class SkipList:
     # Class for Skip list
-    def __init__(self, max_lvl, P):
+    def __init__(self, max_lvl):
         # Maximum level for this skip list
         self.MAXLVL = max_lvl
 
         # P is the fraction of the nodes with level i references also
         # having level i+1 references
-        self.P = P
+        # self.P = P
 
         # create header node and initialize key to -1
         self.header = self.createNode(self.MAXLVL, -1)
@@ -76,10 +94,77 @@ class SkipList:
     # create random level for node
     def randomLevel(self):
         lvl = 0
-        while random.random() < self.P
-            and lvl < self.MAXLVL: lvl += 1
+        while lvl < self.MAXLVL:
+            lvl += 1
         return lvl
 
     # insert given key in skip list
+    def insertElement(self, key):
+        # create update array and initialize it
+        update = [None]*(self.MAXLVL+1)
+        current = self.header
+
+        # start from highest level of skip list move the current reference
+        # forward while key is greater than key of node next to current
+        # Otherwise inserted current in update and move one level down and
+        # continue search
+        for i in range(self.level, -1, -1):
+            while current.forward[i] and current.forward[i].key < key:
+                current = current.forward[i]
+            update[i] = current
+
+        # reached level 0 and forward reference to right, which is desired
+        # position to insert key.
+        current = current.forward[0]
+
+        # If current is NULL that means we have reached to end of the level
+        # or current's key is not equal to key to insert that means we have
+        # to insert node between update[0] and current node
+        if current is None or current.key != key:
+            # Generate a random level for node
+            rlevel = self.randomLevel()
+
+            # if random level is greater than list's current level (node with
+            # highest level inserted in list so far), initialize update value
+            # with reference to header for further use
+            if rlevel > self.level:
+                for i in range(self.level+1, rlevel+1):
+                    update[i] = self.header
+                self.level = rlevel
+
+            # create new node with random level generated
+            n = self.createNode(rlevel, key)
+
+            # insert node by rearranging references
+            for i in range(rlevel+1):
+                n.forward[i] = update[i].forward[i]
+                update[i].forward[i] = n
+
+            print("Successfully inserted key {}".format(key))
+
+    # Display skip list level wise
+    def displayList(self):
+        print("\n*****Skip List*****")
+        head = self.header
+        for lvl in range(self.level+1):
+            print("Level {}: ".format(lvl), end=" ")
+            node = head.forward[lvl]
+            while(node is not None):
+                print(node.key, end=" ")
+                node = node.forward[lvl]
+            print("")
 
 
+if __name__ == "__main__":
+    skipList = SkipList(2)
+    skipList.insertElement(3)
+    skipList.insertElement(6)
+    skipList.insertElement(7)
+    skipList.insertElement(9)
+    skipList.insertElement(12)
+    skipList.insertElement(19)
+    skipList.insertElement(17)
+    skipList.insertElement(26)
+    skipList.insertElement(21)
+    skipList.insertElement(25)
+    skipList.displayList()
---
+++ root/raspberrypi/RaspberryPiPrj/Nokia5110/lcd_stat.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# Copyright (c) 2015
+# Author: Clarence Ho
+#
+# Python script for Raspberry Pi to show stats on Nokia 5110 screen
+#
+# Based on tutorial on Adafruit
+# https://learn.adafruit.com/nokia-5110-3310-lcd-python-library/overview
+import sys
+import re
+import time
+from datetime import datetime
+import psutil
+
+import Adafruit_Nokia_LCD as LCD
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_DHT
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+# Raspberry Pi hardware SPI config:
+DC = 23
+RST = 24
+SPI_PORT = 0
+SPI_DEVICE = 0
+# Raspberry Pi Wiring DHT22
+DHT_PIN = 17
+DHT_SENSOR = Adafruit_DHT.AM2302
+
+# Title|Artist|Album Name
+artist = ""
+track = ""
+album = ""
+
+
+def extract(line):
+    m = re.match('^(Title|Artist|Album Name): \"(.*?)\"\.$', line)
+    if m:
+        return m.group(1), m.group(2)
+    else:
+        return None, None
+
+
+def update(key, val):
+    global artist, album, track
+    if key == "Artist":
+        artist = val
+    elif key == "Album Name":
+        album = val
+    elif key == "Title":
+        track = val
+
+
+def formatByte(b):
+    if b < 1024:
+        return str(b)
+    if b < 1048576:
+        return str(b / 1024) + "K"
+    if b < 1073741824:
+        return str(b / 1048576) + "M"
+    return str(b / 1073741824) + "G"
+
+
+def progressBar(draw, x, y, width, height, percent):
+    fillWidth = width * percent
+    draw.rectangle((x, y, x+width, y+height), outline=0, fill=255)
+    draw.rectangle((x, y, x+fillWidth, y+height), outline=0, fill=0)
+
+
+def logTempHum(temp, hum):
+    with open("/home/pi/THlog.csv", "a") as theFile:
+        theFile.write("{}\t{}\t{}\n".format(
+            time.strftime("%Y-%m-%d %H:%M:%S"), temp, hum))
+        theFile.close()
+
+
+# Hardware SPI usage:
+disp = LCD.PCD8544(
+        DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=500000))
+
+# Software SPI usage (defaults to bit-bang SPI interface):
+# disp = LCD.PCD8544(DC, RST, SCLK, DIN, CS)
+
+# Initialize library.
+disp.begin(contrast=60)
+# disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+logTempHum(temperature, humidity)
+lastLogMin = datetime.now().minute
+
+while True:
+
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+    image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
+
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+    # Draw a white filled box to clear the image.
+    draw.rectangle((0, 0, LCD.LCDWIDTH, LCD.LCDHEIGHT), outline=255, fill=255)
+
+    # Load default font.
+    font = ImageFont.load_default()
+
+    # Alternatively load a TTF font.
+    # Some nice fonts to try: http://www.dafont.com/bitmap.php
+    # font = ImageFont.truetype('fonts/VCR_OSD_MONO_1.001.ttf', 14)
+
+    # Write the stats
+    draw.text((0, 0), time.strftime("%b%d %H:%M:%S"), font=font)
+
+    draw.text((0, 10), "CPU:", font=font)
+    progressBar(
+            draw, 29, 12, LCD.LCDWIDTH - 30, 6, psutil.cpu_percent() / 100.0)
+
+    mem = psutil.virtual_memory()
+    draw.text((0, 18), "MEM:", font=font)
+    progressBar(
+            draw, 29, 20, LCD.LCDWIDTH - 30, 6, float(mem.used) / mem.total)
+
+    net = psutil.net_io_counters()
+    draw.text((0, 26), "NET:{}/{}".format(
+        formatByte(net.bytes_recv), formatByte(net.bytes_sent)), font=font)
+
+    curMin = datetime.now().minute
+    if curMin in [0, 15, 30, 45] and lastLogMin != curMin:
+        lastLogMin = curMin
+        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        logTempHum(temperature, humidity)
+    draw.text((0, 34), "T:{:.0f}*C H:{:.0f}%".format(
+        temperature, humidity), font=font)
+
+    # Display image.
+    disp.image(image)
+    disp.display()
+
+    time.sleep(1)
